4c41742e44c29d058323ccfec59b71d4d4bfe649
吴至波 <Zeno@ZenodeMacBook-Pro.local>
2017-12-05 16:19:44 +0800

    指定优先级

--- TestGCDDemo/TestGCDDemo/ViewController.m
+++ TestGCDDemo/TestGCDDemo/ViewController.m
@@ -57,7 +57,89 @@
         });
     }
 }
+//指定优先级
+- (IBAction)setTargetQueue:(id)sender {
+    
+    NSLog(@"************指定优先级***********");
+    dispatch_queue_t serialDispatchQueue = dispatch_queue_create("com.MisterBooo.TestGCDDemo.www", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t dispatchGetGlboalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+    // 第一个参数为要设置优先级的queue,第二个参数是参照物，既将第一个queue的优先级和第二个queue的优先级设置一样。
+    dispatch_set_target_queue(serialDispatchQueue, dispatchGetGlboalQueue);
+    
+    dispatch_async(serialDispatchQueue, ^{
+        NSLog(@"低优先级喽");
+    });
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"优先级一般1。。。");
+    });
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"优先级一般2。。。");
+    });
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"优先级一般3。。。");
+    });
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"优先级一般4。。。");
+    });
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"优先级一般5。。。");
+    });
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"优先级一般6。。。");
+    });
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"优先级一般7。。。");
+    });
+    
+    /*
+     输出:
+     2017-12-05 16:18:37.738609+0800 TestGCDDemo[99519:4104624] ************指定优先级***********
+     2017-12-05 16:18:37.739033+0800 TestGCDDemo[99519:4104977] 优先级一般1。。。
+     2017-12-05 16:18:37.739061+0800 TestGCDDemo[99519:4104975] 优先级一般2。。。
+     2017-12-05 16:18:37.739057+0800 TestGCDDemo[99519:4104978] 低优先级喽
+     2017-12-05 16:18:37.739371+0800 TestGCDDemo[99519:4104990] 优先级一般3。。。
+     2017-12-05 16:18:37.739647+0800 TestGCDDemo[99519:4104991] 优先级一般4。。。
+     2017-12-05 16:18:37.739780+0800 TestGCDDemo[99519:4104976] 优先级一般5。。。
+     2017-12-05 16:18:37.739923+0800 TestGCDDemo[99519:4104992] 优先级一般6。。。
+     2017-12-05 16:18:37.740032+0800 TestGCDDemo[99519:4104977] 优先级一般7。。。
+     */
+   
+}
 
+- (IBAction)printAllDispacth:(id)sender {
+    NSLog(@"************printAllDispacth***********");
+    
+ /*
+  常见编译警告类型
+  -Wincompatible-pointer-types    指针类型不匹配
+  -Wincomplete-implementation     没有实现已声明的方法
+  -Wprotocol                      没有实现协议的方法
+  -Wimplicit-function-declaration 尚未声明的函数(通常指c函数)
+  -Warc-performSelector-leaks     使用performSelector可能会出现泄漏(该警告在xcode4.3.1中没出现过,网上流传说4.2使用performselector:withObject: 就会得到该警告)
+  -Wdeprecated-declarations       使用了不推荐使用的方法(如[UILabel setFont:(UIFont*)])
+  -Wunused-variable               含有没有被使用的变量
+  -Wundeclared-selector           未定义selector方法
+  
+  */
+    
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+    //被夹在这中间的代码针对于此警告都会无视并且不显示出来
+
+    dispatch_queue_t mainDispatchQueue = dispatch_get_main_queue();
+    
+    dispatch_queue_t globalDispatchQueueHigh = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+    
+    dispatch_queue_t globalDispatchQueueDefault = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    
+    dispatch_queue_t globalDispatchQueueLow = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+    
+    dispatch_queue_t globalDispatchQueueBackground = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
+   
+#pragma clang diagnostic pop
+    
+}
 
 
 //在指定队列添加任务
@@ -83,7 +165,9 @@
     dispatch_async(queue, ^{
         NSLog(@"7:%@",[NSThread currentThread]);
     });
+    
 }
+
 
 
 @end
