177e6cc0c4ea677b82a30f77e8d6df2a678cde1a
吴至波 <Zeno@ZenodeMacBook-Pro.local>
2017-12-06 10:11:45 +0800

    __NSStackBlock__ 和__NSGlobalBlock__

--- TestBlockDemo/TestBlockDemo/ViewController.m
+++ TestBlockDemo/TestBlockDemo/ViewController.m
@@ -17,6 +17,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self printfGlobalBlock];
+    [self printfStackBLock];
+}
+
+- (void)printfGlobalBlock{
+    NSLog(@"%@",^{
+        int temp = 10;
+        printf("%d\n",temp);
+    });
+    //2017-12-05 19:27:53.425680+0800 TestBlockDemo[3924:4248116] <__NSGlobalBlock__: 0x10bc83088>
+}
+- (void)printfStackBLock{
+    int temp = 10;
+    NSLog(@"%@",^{
+        printf("%d\n",temp);
+    });
+    //2017-12-06 10:10:57.147892+0800 TestBlockDemo[5538:4373417] <__NSStackBlock__: 0x7fff53683ba8>
+
 }
 
 
